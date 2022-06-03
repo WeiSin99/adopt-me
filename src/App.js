585c31c43df1a8157086eabde3a35fbe9d419c19
WeiSin99 <looweisin008@gmail.com>
2022-06-03 14:59:58 +0100

    07 react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { render } from 'react-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { StrictMode } from 'react';
 import SearchParams from './SearchParams';
+import Details from './Details';
 
 import './style.css';
 import './index.html';
@@ -8,10 +10,15 @@ import './index.html';
 const App = () => {
   return (
     <StrictMode>
-      <div>
-        <h1>Adopt Me!</h1>
-        <SearchParams />
-      </div>
+      <BrowserRouter>
+        <header>
+          <Link to='/'>Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path='/details/:id' element={<Details />} />
+          <Route path='/' element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
     </StrictMode>
   );
 };
